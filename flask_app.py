3b35ea95cfeb0da60378c42e6ddd7bc9bda21e49
Mahsa Zand khaneh shahri <mzandkhanehs@uni-osnabrueck.de>
2023-11-29 00:25:05 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,33 @@
 from flask import Flask, request, render_template
 from markupsafe import escape
 from whoosh.index import open_dir
-from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser, PhrasePlugin
+from whoosh.scoring import BM25F 
 from config import index_dir
+
 
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('WebHTML.html')
+# How many views we need: first one for homepage and second one for search
+
+@app.route('/') # first view for showing the homepage 
+def home(): # for homepage
+    return render_template('WebHTML.html') 
 
 @app.route('/search')
 def search():
     query = request.args.get('q', '')
     ix = open_dir(index_dir)
-    qp = QueryParser("content", schema=ix.schema)
+   
+    qp = MultifieldParser(["title", "content"], schema=ix.schema)
+    qp.add_plugin(PhrasePlugin())  # Add support for phrase searching
+
     q = qp.parse(escape(query))
-    
+
     results_list = []
-    with ix.searcher() as searcher:
+    with ix.searcher(weighting=BM25F(B=0.5, K1=1.5)) as searcher:
         results = searcher.search(q)
         for result in results:
             results_list.append({
@@ -28,7 +35,7 @@ def search():
                 'title': result['title'],
                 'teaser': result['teaser']
             })
-    print(results_list)
+
     return render_template('ResultHTML.html', results_list=results_list)
 
 if __name__ == '__main__':
